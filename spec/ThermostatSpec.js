227972a7b7a66bf229f5168812f68f1b8c50d2e0
Evelyn <evelyn@ipsoft.es>
2019-04-03 11:29:01 +0100

    Created and passed tests for the first two requirements - Thermostat starts at 20 degress and can increase the temp with an up function

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,18 @@
+describe('Thermostat', function() { 
+ 
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  })
+
+  it('Shows initial temperature of 20 degrees', function() {
+    expect( thermostat.temperature ).toEqual(20);
+  });
+
+  it('Can increase temperature by 1 degree', function up() {
+    thermostat.up();
+    expect( thermostat.temperature ).toEqual(21);
+  });
+
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,21 @@
+// var Thermostat = function(){};
+
+// Thermostat.prototype.temperature = 20;
+
+// Thermostat.prototype.up = function up(){
+//   this.temperature ++;
+// }; 
+// The lines above cover the 2 fisrt specifications 
+// of the thermostat program, which are:
+// 1.- Thermostat starts at 20 degrees
+// 2.- Can increase temperature with an up function
+// And then, a better way to do this, from line 9 to 13:
+
+var Thermostat = function(){
+  this.temperature = 20;
+
+  this.up = function up(){
+    this.temperature ++;
+  };
+};
+
